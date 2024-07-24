1bad672a23f57754dcefb0b43e14eccae268fe6e
jsn0509 <jsn_@naver.com>
2024-07-25 00:48:49 +0900

    design: 불필요한 스타일 제거

--- src/components/WebView/styles.ts
+++ src/components/WebView/styles.ts
@@ -4,10 +4,6 @@ import styled, { css } from "styled-components";
 export const StyledWebView = styled(ParentWebView).withConfig({
   shouldForwardProp: (prop) => !["windowWidth", "windowHeight"].includes(prop)
 })<{ windowWidth?: number; windowHeight?: number }>`
-  flex: 1;
-  width: 100%;
-  height: 100%;
-
   ${({ windowWidth, windowHeight }) => css`
     width: ${windowWidth}px;
     height: ${windowHeight}px;
