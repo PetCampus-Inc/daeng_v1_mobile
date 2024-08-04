a623861bb16429fabecb3a11245b3ec3db94c8b0
jsn0509 <jsn_@naver.com>
2024-08-05 02:27:53 +0900

    chore: axios 라이브러리 추가 및 getSignInMethod 변경

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,10 +1,9 @@
 import { apiUrl } from "~/config/url";
+import { request } from "~/libs/request";
 import { SignInMethod } from "~/types/auth.types";
 
-export const getSignInMethod = async (uniqueId: string): Promise<SignInMethod> => {
-  const queryString = new URLSearchParams({ id: uniqueId }).toString();
-  console.log("API 호출: ", `${apiUrl}/auth/sign-in-method/${queryString}`);
-  // const response = fetch(`${apiUrl}/auth/sign-in-method/${queryString}` )
-  // return response
-  return "kakao" as SignInMethod;
+export const getSignInMethod = async (deviceId: string): Promise<SignInMethod> => {
+  const url = `${apiUrl}member/firebase/provider`;
+  const { data } = await request<SignInMethod>({ url, params: { deviceId } });
+  return data;
 };
