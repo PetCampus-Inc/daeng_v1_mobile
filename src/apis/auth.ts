748d27e47f1a4a3c276c2602b466a47d08100a64
jsn0509 <jsn_@naver.com>
2024-08-07 01:21:35 +0900

    feat: 로그인 API 추가

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,6 +1,25 @@
+import axios from "axios";
+
 import { apiUrl } from "~/config/url";
-import { request } from "~/libs/request";
+import { request, Response } from "~/libs/request";
 import { FirebaseProvider } from "~/types/auth.types";
+import { MemberRole } from "~/types/role.types";
+
+export const postMemberLogin = async (req: {
+  idToken: string;
+  deviceId: string;
+}): Promise<{ role: MemberRole; accessToken: string }> => {
+  const url = `${apiUrl}member/firebase/login`;
+
+  const { headers, data } = await axios.post<Response<MemberRole>>(url, {
+    idToken: req.idToken,
+    deviceId: req.deviceId
+  });
+
+  const token = headers.authorization;
+  const accessToken = token.startsWith("Bearer ") ? token.slice(7) : token;
+  return { role: data.data, accessToken };
+};
 
 export const getFirebaseProvider = async (deviceId: string): Promise<FirebaseProvider> => {
   const url = `${apiUrl}member/firebase/provider`;
