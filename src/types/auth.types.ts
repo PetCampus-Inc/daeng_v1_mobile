c53e33cb64d6584d88051283928fc7487fc71296
jsn0509 <jsn_@naver.com>
2024-08-09 05:35:48 +0900

    feat: 로그인 된 user 데이터 recoil 관리 로직 추가

--- src/types/auth.types.ts
+++ src/types/auth.types.ts
@@ -1,6 +1,14 @@
+import { Role } from "~/types/role.types";
+
 export type FirebaseProvider = "GOOGLE" | "APPLE" | "KAKAO";
 
 export interface FirebaseAuthData {
   idToken: string;
   deviceId: string;
+}
+
+export interface UserInfo {
+  isLogin: boolean;
+  accessToken: string;
+  role: Role;
 }
