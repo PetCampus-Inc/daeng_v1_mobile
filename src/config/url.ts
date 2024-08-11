bc2d62bcf66b91a216d5bc290fac9776bc1b69c8
jsn0509 <jsn_@naver.com>
2024-08-11 21:12:46 +0900

    chore: 테스트 앱 배포를 위해 도메인 변경

--- src/config/url.ts
+++ src/config/url.ts
@@ -7,7 +7,7 @@ import {
 import { Platform } from "react-native";
 
 const devBaseUrl = Platform.OS === "ios" ? DEV_IOS_SERVER_DOMAIN : DEV_ANDROID_SERVER_DOMAIN;
-const baseUrl = __DEV__ ? devBaseUrl : SERVER_DOMAIN;
+const baseUrl = SERVER_DOMAIN; //__DEV__ ? devBaseUrl : SERVER_DOMAIN;
 
 const apiUrl = `${baseUrl}${SERVER_API_PATH}`;
 
