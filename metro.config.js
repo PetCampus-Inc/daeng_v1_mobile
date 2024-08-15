a6e579257c0bc2abca498a63a1934bdc942a4b4d
jsn0509 <jsn_@naver.com>
2024-08-15 18:08:33 +0900

    chore: Yarn v1 > pnpm 마이그레이션

---
+++ metro.config.js
@@ -0,0 +1,37 @@
+const { getDefaultConfig } = require("@react-native/metro-config");
+const { makeMetroConfig } = require("@rnx-kit/metro-config");
+const MetroSymlinksResolver = require("@rnx-kit/metro-resolver-symlinks");
+const { withNativeWind } = require("nativewind/metro");
+
+const defaultConfig = getDefaultConfig(__dirname);
+const { assetExts, sourceExts } = defaultConfig.resolver;
+
+/**
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = makeMetroConfig({
+  ...defaultConfig,
+  transformer: {
+    ...defaultConfig.transformer,
+    babelTransformerPath: require.resolve("react-native-svg-transformer"),
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true
+      }
+    })
+  },
+  resolver: {
+    ...defaultConfig.resolver,
+    resolveRequest: MetroSymlinksResolver(),
+    assetExts: assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...sourceExts, "svg"]
+  }
+});
+
+module.exports = withNativeWind(config, {
+  input: "./src/styles/global.css"
+});
