b55359861c47751f30160abd6056cd63fe185a48
jsn0509 <jsn_@naver.com>
2024-08-25 04:22:45 +0900

    refactor: 메세지 권한 로직 개선

--- src/permission/messaging.ts
+++ src/permission/messaging.ts
@@ -1,3 +1,4 @@
+import notifee from "@notifee/react-native";
 import messaging from "@react-native-firebase/messaging";
 import { Platform, PermissionsAndroid } from "react-native";
 
@@ -5,33 +6,17 @@ const firebaseMessaging = messaging();
 
 export const requestMessagingPermission = async () => {
   try {
-    if (Platform.OS === "ios") await iosRequestPermission();
-    else if (Platform.OS === "android") await androidRequestPermission();
-  } catch (error) {
-    console.error("[Permission]", error);
-  }
-};
+    const hasPermission = await notifee.requestPermission();
+    if (hasPermission) return;
 
-// IOS
-export const iosRequestPermission = async () => {
-  try {
-    const authStatus = await firebaseMessaging.requestPermission();
-    const enabled =
-      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
-    if (!enabled) throw new Error("알림 권한이 거부되었습니다.");
-  } catch (error) {
-    throw error;
-  }
-};
-
-// Android
-export const androidRequestPermission = async () => {
-  if (Platform.OS !== "android") return;
-
-  try {
-    // API 레벨 33 이상
-    if (Platform.Version >= 33) {
+    if (Platform.OS === "ios") {
+      const authStatus = await firebaseMessaging.requestPermission();
+      const enabled =
+        authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+        authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+      if (!enabled) throw new Error("알림 권한이 거부되었습니다.");
+    } else if (Platform.OS === "android" && Platform.Version >= 33) {
+      // API 레벨 33 이상
       const granted = await PermissionsAndroid.request(
         PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS
       );
@@ -40,6 +25,6 @@ export const androidRequestPermission = async () => {
         throw new Error("알림 권한이 거부되었습니다.");
     }
   } catch (error) {
-    throw error;
+    console.error("[Permission]", error);
   }
 };
