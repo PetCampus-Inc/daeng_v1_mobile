a37b2215213f401e1a69e6d5911fcf0acd1abb69
jsn0509 <jsn_@naver.com>
2024-09-23 20:11:04 +0900

    chore: BaseURL 안드로이드 URL 제거

--- src/shared/config/domain.ts
+++ src/shared/config/domain.ts
@@ -1,24 +1,6 @@
-import {
-  DEV_SERVER_DOMAIN,
-  SERVER_DOMAIN,
-  SERVER_API_PATH,
-  DEV_SERVER_DOMAIN_NOT_EMULATOR
-} from "@env";
-import { NativeModules, Platform } from "react-native";
+import { DEV_SERVER_DOMAIN, SERVER_DOMAIN, SERVER_API_PATH } from "@env";
 
-const isIOS = Platform.OS === "ios";
-const isAndroidEmulator = () => {
-  return (
-    (Platform.OS === "android" &&
-      NativeModules.DetectEmulator &&
-      NativeModules.DetectEmulator.isEmulator) ||
-    false
-  );
-};
-
-const androidDevUrl = isAndroidEmulator() ? DEV_SERVER_DOMAIN : DEV_SERVER_DOMAIN_NOT_EMULATOR;
-const devUrl = isIOS ? DEV_SERVER_DOMAIN : androidDevUrl;
-const baseUrl = __DEV__ ? devUrl : SERVER_DOMAIN;
+const baseUrl = __DEV__ ? DEV_SERVER_DOMAIN : SERVER_DOMAIN;
 const apiUrl = `${baseUrl}/${SERVER_API_PATH}`;
 
 console.info(`[Base Domain] ${baseUrl}`);
