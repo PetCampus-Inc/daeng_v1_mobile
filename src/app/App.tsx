07f11562419263b5caaf14ce9b90b5170e646419
jsn0509 <jsn_@naver.com>
2024-09-26 15:04:03 +0900

    fix: App 네비게이션 추가

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,15 @@
+import { NavigationContainer } from "@react-navigation/native";
 import { SafeAreaView } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { RecoilRoot } from "recoil";
 import { ReadableStream } from "web-streams-polyfill";
 
-import { HomeScreen } from "@_screens/home/ui/HomeScreen";
+import { RootNavigation } from "@_app/navigation/RootNavigation";
 
-import "./styles/global.css";
 import "react-native-url-polyfill/auto";
 import "react-native-get-random-values";
+
+import "./styles/global.css";
 
 if (typeof global.ReadableStream === "undefined") {
   global.ReadableStream = ReadableStream as unknown as typeof global.ReadableStream;
@@ -18,7 +20,11 @@ function App(): React.JSX.Element {
     <RecoilRoot>
       <SafeAreaProvider>
         <SafeAreaView className="flex-1">
-          <HomeScreen />
+          <NavigationContainer>
+            <RootNavigation />
+          </NavigationContainer>
+
+          {/* <HomeScreen /> */}
         </SafeAreaView>
       </SafeAreaProvider>
     </RecoilRoot>
