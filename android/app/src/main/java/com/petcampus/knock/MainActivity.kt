4cd9d4b03eaa1662801c90320322a0e3796050e5
jsn0509 <jsn_@naver.com>
2024-09-26 18:05:35 +0900

    hotfix/chore: 번들 ID 변경

--- android/app/src/main/java/com/petcampus/knock/MainActivity.kt
+++ android/app/src/main/java/com/petcampus/knock/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.petcampus.knock
+package net.knockdog.petcampus
 
 import android.os.Bundle;
 import com.facebook.react.ReactActivity
@@ -13,7 +13,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "com.petcampus.knock"
+  override fun getMainComponentName(): String = "net.knockdog.petcampus"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
