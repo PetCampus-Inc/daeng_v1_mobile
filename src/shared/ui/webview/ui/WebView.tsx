ca5a49bfde2ed40a91dfdc3f465b777f38926a95
jsn0509 <jsn_@naver.com>
2024-10-13 04:12:03 +0900

    hotfix: WebView 중복 옵션 제거

--- src/shared/ui/webview/ui/WebView.tsx
+++ src/shared/ui/webview/ui/WebView.tsx
@@ -39,7 +39,6 @@ export const WebView = forwardRef<ParentWebView, WebViewProps>(
         injectedJavaScript={debuggingScript}
         allowsInlineMediaPlayback
         webviewDebuggingEnabled={__DEV__}
-        allowsInlineMediaPlayback
         /** 키보드 액세서리 뷰 숨기기 */
         hideKeyboardAccessoryView={true}
         /** 쿠키 공유 활성화 */
