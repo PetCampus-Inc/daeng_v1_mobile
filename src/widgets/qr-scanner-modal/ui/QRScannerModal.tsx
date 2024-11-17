61e66ae6caa3a39a29473499c3c5632acf68a68e
jsn0509 <jsn_@naver.com>
2024-11-18 03:42:17 +0900

    feat: 카메라 오버레이, 손전등 기능 추가

--- src/widgets/qr-scanner-modal/ui/QRScannerModal.tsx
+++ src/widgets/qr-scanner-modal/ui/QRScannerModal.tsx
@@ -1,12 +1,20 @@
-import { Modal, ModalProps, StyleSheet, View } from "react-native";
+import { useState } from "react";
+import { Modal, ModalProps, StyleSheet, TouchableHighlight, View } from "react-native";
 import { Camera, useCameraDevice, useCodeScanner } from "react-native-vision-camera";
+
+import { ScannerOverlay } from "@_widgets/qr-scanner-modal/ui/SacnnerOverlay";
+
+import CloseIcon from "@_shared/assets/svg/close.svg";
+import TorchIcon from "@_shared/assets/svg/torch.svg";
 
 interface QRScannerModalProps extends ModalProps {
   onScanned?: (code: string) => void;
+  onClose?: () => void;
 }
 
-const CodeScannerModal = ({ visible, onScanned }: QRScannerModalProps) => {
+const CodeScannerModal = ({ visible, onScanned, onClose }: QRScannerModalProps) => {
   const device = useCameraDevice("back");
+  const [torch, setTorch] = useState(false);
 
   const codeScanner = useCodeScanner({
     codeTypes: ["qr"],
@@ -22,17 +30,34 @@ const CodeScannerModal = ({ visible, onScanned }: QRScannerModalProps) => {
     <Modal visible={visible}>
       <View className="flex-1">
         {device ? (
-          <Camera
-            style={StyleSheet.absoluteFill}
-            device={device}
-            codeScanner={codeScanner}
-            isActive={true}
-            photo={true}
-          />
+          <>
+            <Camera
+              style={StyleSheet.absoluteFill}
+              device={device}
+              codeScanner={codeScanner}
+              isActive={true}
+              photo={true}
+              torch={torch ? "on" : "off"}
+            />
+            <ScannerOverlay />
+          </>
         ) : (
           <View />
         )}
       </View>
+      <TouchableHighlight
+        className="absolute top-safe-or-0 right-4 z-10 p-2 rounded-full"
+        onPress={() => onClose?.()}
+      >
+        <CloseIcon className="opacity-80" />
+      </TouchableHighlight>
+
+      <TouchableHighlight
+        className="absolute left-1/2 -translate-x-1/2 bottom-safe-or-20 z-10 p-4 rounded-full bg-foreground opacity-50"
+        onPress={() => setTorch((prev) => !prev)}
+      >
+        <TorchIcon />
+      </TouchableHighlight>
     </Modal>
   );
 };
