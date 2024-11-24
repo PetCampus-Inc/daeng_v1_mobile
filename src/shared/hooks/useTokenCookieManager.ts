a683738e8a3854a947c94c1a5de9f2ed41a0301d
jsn0509 <jsn_@naver.com>
2024-11-24 19:13:30 +0900

    refactor: 경로 변경 및 RefreshToken 라이브러리로 추상화

--- src/shared/hooks/useTokenCookieManager.ts
+++ src/shared/hooks/useTokenCookieManager.ts
@@ -5,22 +5,8 @@ import EncryptedStorage from "react-native-encrypted-storage";
 import { baseUrl } from "@_shared/config/domain";
 import { REFRESH_TOKEN_KEY } from "@_shared/constants/storage";
 
-/**
- * 자동 로그인을 위한 훅입니다. `EncryptedStorage`에 `Refresh-Token`이 있다면 쿠키에 저장합니다.
- * @returns `extractAndSaveRefreshToken` 쿠키에서 `Refresh-Token` 추출 후 스토리지에 저장합니다.
- */
+/** 자동 로그인을 위한 훅입니다. `EncryptedStorage`에 `Refresh-Token`이 있다면 쿠키에 저장합니다. */
 export const useTokenCookieManager = () => {
-  /** 쿠키에서 `Refresh-Token` 추출 후 스토리지에 저장합니다. */
-  const extractAndSaveRefreshToken = async () => {
-    const cookies = await CookieManager.get(baseUrl);
-    const refreshToken = cookies.refreshToken;
-
-    if (!refreshToken) return;
-
-    await EncryptedStorage.setItem(REFRESH_TOKEN_KEY, refreshToken.value);
-  };
-
-  /** 쿠키에 `Refresh-Token`을 저장합니다. */
   useEffect(() => {
     const setCookieRefreshToken = async () => {
       const refreshToken = await EncryptedStorage.getItem(REFRESH_TOKEN_KEY);
@@ -37,6 +23,4 @@ export const useTokenCookieManager = () => {
 
     setCookieRefreshToken();
   }, []);
-
-  return { extractAndSaveRefreshToken };
 };
