40222b55bbba4a68c5951fc88cfb727f7cce18bf
jsn0509 <jsn_@naver.com>
2024-11-24 19:21:02 +0900

    fix: 삭제 누락된 파일 삭제

--- src/shared/utils/get-fcm-token/getFcmToken.ts
+++
@@ -1,7 +0,0 @@
-import messaging from "@react-native-firebase/messaging";
-
-export const getFcmToken = async () => {
-  const fcmToken = await messaging().getToken();
-  if (fcmToken) return fcmToken;
-  else throw new Error("FCM 토큰을 가져올 수 없습니다.");
-};
