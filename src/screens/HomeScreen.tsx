96d3f5a99f9566aab3c36c0d7e9e1c37d33a467b
jsn0509 <jsn_@naver.com>
2024-12-07 18:13:21 +0900

    refactor: 불필요한 로직 제거

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,18 +1,15 @@
 import React, { useRef } from "react";
 import SplashScreen from "react-native-splash-screen";
-import WebView, { WebViewNavigation } from "react-native-webview";
-import { useSetRecoilState } from "recoil";
+import WebView from "react-native-webview";
 
 import { BridgeWebView } from "@_widgets/bridge-webview";
 
 import { useNotificationRouting } from "@_shared/hooks/use-push-notification";
 import { useBackHandler } from "@_shared/hooks/useBackHandler";
 import { useTokenCookieManager } from "@_shared/hooks/useTokenCookieManager";
-import { webRouteState } from "@_shared/store/webRouteState";
 
 export const HomeScreen = () => {
   const webviewRef = useRef<WebView>(null);
-  const setWebRoute = useSetRecoilState(webRouteState);
 
   useBackHandler();
   useTokenCookieManager();
@@ -21,18 +18,5 @@ export const HomeScreen = () => {
       webviewRef.current?.injectJavaScript(`window.location.href = '${route}';`)
   });
 
-  const handleNavigationStateChange = (nav: WebViewNavigation) => {
-    setWebRoute({
-      url: nav.url,
-      canGoBack: nav.canGoBack
-    });
-  };
-
-  return (
-    <BridgeWebView
-      ref={webviewRef}
-      onLoadEnd={() => SplashScreen.hide()}
-      onNavigationStateChange={handleNavigationStateChange}
-    />
-  );
+  return <BridgeWebView ref={webviewRef} onLoadEnd={() => SplashScreen.hide()} />;
 };
