dd8867de031350d38dd533a38f8f78fe3dd2e72e
jsn0509 <jsn_@naver.com>
2025-02-27 02:28:54 +0900

    refactor: 네이티브 스크린 푸시 메서드 추가

--- src/shared/hooks/use-stack-navigation/useStackNavigation.ts
+++ src/shared/hooks/use-stack-navigation/useStackNavigation.ts
@@ -1,31 +1,32 @@
-import { useNavigation, useNavigationState } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 
 import { RootStackParamList } from "@_app/navigation/RootNavigation";
 
-type NavigateType = "push" | "replace" | "back" | "pop";
+type NavigateMethod = "webview" | "push" | "replace" | "back" | "pop";
 
 export type NavigateOptions = {
-  type: NavigateType;
+  method: NavigateMethod;
   path: string;
   state?: any;
 };
 
 export const useStackNavigation = () => {
-  const { replace, push, pop, popToTop, canGoBack } =
+  const { name } = useRoute();
+  const { push, pop, popToTop, canGoBack, replace } =
     useNavigation<NativeStackNavigationProp<RootStackParamList>>();
 
-  const currentRoute = useNavigationState((state) => state.routes[state.routes.length - 1]);
-
-  return ({ type, path, state }: NavigateOptions) => {
-    const currentScreen = currentRoute.name as keyof RootStackParamList;
-
-    switch (type) {
+  return ({ method, path, state }: NavigateOptions) => {
+    switch (method) {
       case "push":
+        if (isStackScreen(path)) push(path);
+        else throw new Error("존재하지 않는 경로입니다.");
+        break;
+      case "webview":
         push("Stack", { path, state });
         break;
       case "replace":
-        replace(currentScreen, { path, state });
+        replace(name as keyof RootStackParamList, { path, state });
         break;
       case "back":
         if (canGoBack()) pop();
@@ -37,3 +38,10 @@ export const useStackNavigation = () => {
     }
   };
 };
+
+/** 이동 가능한 네이티브 스크린 리스트 */
+const NATIVE_SCREENS: (keyof RootStackParamList)[] = [];
+
+function isStackScreen(path: string): path is keyof RootStackParamList {
+  return NATIVE_SCREENS.includes(path as keyof RootStackParamList);
+}
