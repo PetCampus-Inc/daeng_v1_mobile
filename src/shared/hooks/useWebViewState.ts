f69b2c0322adf69e4652995f1602aff6ead2a0f7
jsn0509 <jsn_@naver.com>
2025-02-27 02:28:10 +0900

    fix: 스크린 이름 변경 누락 픽스

--- src/shared/hooks/useWebViewState.ts
+++ src/shared/hooks/useWebViewState.ts
@@ -4,18 +4,23 @@ import WebView from "react-native-webview";
 
 import { RootStackParamList } from "@_app/navigation/RootNavigation";
 
-import { postMessage } from "@_shared/lib/bridge/lib/postMessage";
+import { postMessage } from "@_shared/lib/bridge";
 
 interface WebViewStateOptions {
   webviewRef: RefObject<WebView>;
 }
 
+/**
+ * WebView에서 전달한 상태 값을 처리하는 훅
+ * @param webviewRef WebView 참조
+ */
 export const useWebViewState = ({ webviewRef }: WebViewStateOptions) => {
   const {
     params: { state }
-  } = useRoute<RouteProp<RootStackParamList, "WebView">>();
+  } = useRoute<RouteProp<RootStackParamList, "Stack">>();
 
   const onLoadEnd = () => {
+    // WebView 로드가 완료되면 WebView로 상태 값 전달
     if (state) {
       postMessage(webviewRef, {
         type: "WEBVIEW_STATE",
